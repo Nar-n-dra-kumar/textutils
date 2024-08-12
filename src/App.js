76d05dab9575f440ad1c01efd2cc0c924985cae0
Nar-n-dra-kumar <narendraboby9390@gmail.com>
2024-08-12 11:24:08 +0530

    Router-setup,link-to

--- src/App.js
+++ src/App.js
@@ -2,54 +2,55 @@ import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import About from './components/About';
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Alert from './components/Alert';
-
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 
 function App() {
-  const [mode , setMode] = useState('warning'); //weather dark mode enabled or not.....
-  const [alert, setAlert] = useState('null');
+  const [mode, setMode] = useState('warning'); // Whether dark mode is enabled or not
+  const [alert, setAlert] = useState(null);
 
-const showAlert =(message , type) =>{
-  setAlert({
-    msg: message,
-    type: type,
-
-  })
-  setTimeout(() => {
-    setAlert(null);
-  }, 1500); 
-  
-}
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  }
 
   const toggleMode = () => {
     if (mode === 'dark') {
       setMode('warning');
       document.body.style.backgroundColor = '#fff';
-      document.body.style.color = 'black'; 
-      showAlert("Light mode has been enabled" , "success");
-    } 
-    else {
+      document.body.style.color = 'black';
+      showAlert("Light mode has been enabled", "success");
+    } else {
       setMode('dark');
-      document.body.style.backgroundColor = 'rgb(2 38 74)';  //light mode color...  replace with your color..^^ ^^
-      document.body.style.color = '#fff';  //light mode text color...  replace with your color..^^ ^^
-      showAlert("Dark mode has been enabled" , "success");
-
+      document.body.style.backgroundColor = 'rgb(2 38 74)'; // Replace with your preferred color
+      document.body.style.color = '#fff'; // Replace with your preferred color
+      showAlert("Dark mode has been enabled", "success");
     }
-
   }
   return (
     <>
-    <Navbar title="React" home="Home" about="About" mode={mode} toggleMode={toggleMode}/>
-    <Alert alert={alert} />
-    <div className="container"> 
-      <TextForm showAlert={showAlert} hedding="ENTER TEXT TO ANALYZE" mode={mode} />
-    </div>
-    <About mode={mode} />
-   </>
-   
+      <Router>
+        <Navbar title="React" home="Home" about="About" mode={mode} toggleMode={toggleMode} />
+        <Alert alert={alert} />
+        <div className="container">
+          <Routes>
+            <Route path="/about" element={<About />} />
+            <Route path="/" element={<TextForm showAlert={showAlert} heading="ENTER TEXT TO ANALYZE" mode={mode} />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
   );
 }
-
 
 export default App;
